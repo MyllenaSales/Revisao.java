3ca57af17f88559001796fdaa954625a6ab4ad66
MyllenaSales <160550269+MyllenaSales@users.noreply.github.com>
2024-02-22 07:38:14 -0300

    Update Exercicio3.java

--- Aula1/src/revisao/Exercicio3.java
+++ Aula1/src/revisao/Exercicio3.java
@@ -5,11 +5,11 @@ public class Exercicio3 {
 	private int numero1=0;
 	private int numero2=1;
 	private int numero;
-	private int rep=0;
+	private int rep=1;
 	
 	public void sequencia() {
 		
-		while(rep<=101) {
+		while(rep<=48) {
 			
 			numero = numero1+numero2;
 			System.out.print(numero1+", "+numero2+", "+numero+", ");
