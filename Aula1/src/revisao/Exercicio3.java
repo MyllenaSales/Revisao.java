5b9cbb34d08f45e76dc3b2d688a123763d1a5c37
MyllenaSales <160550269+MyllenaSales@users.noreply.github.com>
2024-02-23 14:21:52 -0300

    Update Exercicio3.java

--- Aula1/src/revisao/Exercicio3.java
+++ Aula1/src/revisao/Exercicio3.java
@@ -2,23 +2,22 @@ package revisao;
 
 public class Exercicio3 {
 	
-	private int numero1=0;
+	private int numero1=1;
 	private int numero2=1;
-	private int numero;
-	private int rep=1;
+	private int numero=0;
+	private int rep=0;
 	
 	public void sequencia() {
-		
+
+		System.out.print(numero+", ");
 		while(rep<=5) {
+
+			System.out.print(numero1+", "+numero2+", ");
+			numero1 = numero1+numero2;
+			numero2 = numero1+numero2;
 			
-			numero = numero1+numero2;
-			System.out.print(numero1+", "+numero2+", "+numero+", ");
-			numero1=numero2+numero;
-			numero2 =numero+numero1;
-		
 			rep++;
 		}
-		
 	}
 
 }
